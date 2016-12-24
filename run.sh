8bb4bd9be9825188981900c7b9fa50c96ab965cc
Orabig <benoit.chauvet@gmail.com>
2016-12-24 02:08:48 +0100

    Improved version

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 PORT=80
 
 ./build.sh
+mkdir $DIR/users
+mkdir $DIR/cache
+chmod a+w $DIR/users
+chmod a+w $DIR/cache
+
 docker rm -f cgpuzzle-prod
 docker run --restart=always --name=cgpuzzle-prod -d -p $PORT:80 \
     -v $DIR/users:/var/www/users \
