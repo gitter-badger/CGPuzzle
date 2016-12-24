46f6a191c469b8a8672bf466a06688d44e6f94d2
Orabig <benoit.chauvet@gmail.com>
2016-12-24 02:00:25 +0100

    Added permanent cache dirs when running production webapp

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ PORT=80
 ./build.sh
 docker rm -f cgpuzzle-prod
 docker run --restart=always --name=cgpuzzle-prod -d -p $PORT:80 \
+    -v $DIR/users:/var/www/users \
+    -v $DIR/cache:/var/www/cache \
     cgpuzzle
